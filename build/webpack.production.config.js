79a8f5330419ddba16430cf34dbe9e83aa2075cc
dqopb <swancpp89@gmail.com>
2018-05-12 15:42:08 +0900

    fix: ビルドファイルにhashを付与しないようにし、不要になったコマンドを削除した。

--- build/webpack.production.config.js
+++ build/webpack.production.config.js
@@ -1,20 +1,13 @@
 const webpack = require('webpack')
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./webpack.base.config')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = () => {
   return webpackMerge(baseConfig(), {
     mode: 'production',
-    output: {
-      filename: '[name].[hash].js'
-    },
     plugins: [
       new OptimizeCSSAssetsPlugin({}),
-      new MiniCssExtractPlugin({
-        filename: '[name].[hash].css'
-      }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       })
